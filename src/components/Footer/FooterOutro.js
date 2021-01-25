d12bdcb7c5016ede315a2a79fa9f7345b029fc71
Arno van Staden <arnovanstaden@gmail.com>
2021-01-25 16:04:54 +0200

    update about responsive

--- src/components/Footer/FooterOutro.js
+++ src/components/Footer/FooterOutro.js
@@ -16,20 +16,24 @@ const FooterOutro = (props) => {
 
         return (
             <div className={footerQuoteClass}>
-                <h1>“</h1>
-                {props.quote.text}
-                <p>- {props.quote.author}</p>
+                <div className="container">
+                    <h1>“</h1>
+                    {props.quote.text}
+                    <p>- {props.quote.author}</p>
+                </div>
                 <div className={styles.outroCircle}></div>
             </div>
         )
     } else {
         return (
             <div className={footerQuoteClass}>
-                <h1>{props.default.heading}</h1>
-                <p>{props.default.text}</p>
-                <button className="button">
-                    <Link to={props.default.to}>{props.default.buttonText}</Link>
-                </button>
+                <div className="container">
+                    <h1>{props.default.heading}</h1>
+                    <p>{props.default.text}</p>
+                    <button className="button">
+                        <Link to={props.default.to}>{props.default.buttonText}</Link>
+                    </button>
+                </div>
                 <div className={styles.outroCircle}></div>
             </div>
         )
