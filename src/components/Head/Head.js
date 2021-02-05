f536278556e86573e20f009c2520ae79047c4454
Arno van Staden <arnovanstaden@gmail.com>
2021-02-05 13:52:15 +0200

    update canonical & social image

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -11,7 +11,7 @@ const Head = ({ pageMeta }) => {
 
             <title>{pageMeta.title}</title>
             <meta name="description" content={pageMeta.description} />
-            <link rel="canonical" href={pageMeta.canonical && `https://www.t-t-s.co.za/${pageMeta.canonical.toLowerCase()}`} />
+            <link rel="canonical" href={pageMeta.canonical && `https://www.t-t-s.co.za${pageMeta.canonical.toLowerCase()}`} />
             <meta name="robots" content={pageMeta.robots ? pageMeta.robots : "index, follow"} />
 
             <meta name="author" content="Webdacity" />
@@ -23,8 +23,8 @@ const Head = ({ pageMeta }) => {
             <meta property="og:title" content={pageMeta.title} />
             <meta property="og:description" content={pageMeta.description} />
             <meta property="og:type" content="Website" />
-            <meta property="og:url" content={pageMeta.canonical && `https://www.t-t-s.co.za/${pageMeta.canonical.toLowerCase()}`} />
-            <meta property="og:image" content="https://www.t-t-s.co.za//social.png" />
+            <meta property="og:url" content={pageMeta.canonical && `https://www.t-t-s.co.za${pageMeta.canonical.toLowerCase()}`} />
+            <meta property="og:image" content="https://www.t-t-s.co.za/social.png" />
             <meta property="og:image:type" content="image/png" />
             <meta property="og:image:width" content="500" />
             <meta property="og:image:height" content="500" />
